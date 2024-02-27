17b257707ad85383b55925088de582c65bf7c30b
yuchen <02645952@yto.net.cn>
2024-02-27 14:57:12 +0800

    第n次提交

---
+++ ms/src/main/java/demo01.java
@@ -0,0 +1,15 @@
+package ms.src.main.java;
+
+/**
+ * @Author ZYC
+ * @Date 2024-02-27 下午 2:51
+ * @Title: demo01
+ * @Package ms.src.main.java
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+}
