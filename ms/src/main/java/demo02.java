ccdabe57b7b32a62704474c8b0f0d34e847cfd1a
yuchen <02645952@yto.net.cn>
2024-02-27 15:11:25 +0800

    第n+1次提交 多线程测试02

---
+++ ms/src/main/java/demo02.java
@@ -0,0 +1,33 @@
+package ms.src.main.java;
+
+/**
+ * @Author ZYC
+ * @Date 2024-02-27 下午 3:05
+ * @Title: demo02
+ * @Package ms.src.main.java
+ */
+public class demo02 {
+    public static void main(String[] args) {
+        Thread1 mTh1 = new Thread1("A");
+        Thread1 mTh2 = new Thread1("B");
+        mTh1.start();
+        mTh2.start();
+    }
+}
+
+class Thread1 extends Thread {
+    private String name;
+    public Thread1(String name) {
+        this.name = name;
+    }
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(name + "运行 : " + i);
+            try {
+                sleep((int) Math.random() * 10);
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
