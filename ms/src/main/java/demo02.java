25712692ea0c984d0ff80092a6f90f4caa51effb
yuchen <02645952@yto.net.cn>
2024-02-27 15:32:13 +0800

    第n+2次提交 多线程测试03

--- ms/src/main/java/demo02.java
+++ ms/src/main/java/demo02.java
@@ -11,7 +11,7 @@ public class demo02 {
         Thread1 mTh1 = new Thread1("A");
         Thread1 mTh2 = new Thread1("B");
         mTh1.start();
-        mTh2.start();//启动
+        mTh2.start();
     }
 }
 
